c2fad1efd0d950d581342deafa8ad55d171a81a9
PONCEJE <ponceje@oocl.com>
2020-10-19 17:47:31 +0800

    [ParkingLot Story6] [Jeyem] Refactor: in getParkingLot, use findFirst instead of findAny and used Optional.ofNullable for fetch

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,6 +1,8 @@
 package com.oocl.cultivation;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public class ParkingBoy {
     public static final String PLEASE_PROVIDE_YOUR_PARKING_TICKET = "Please provide your parking ticket";
@@ -18,17 +20,15 @@ public class ParkingBoy {
     }
 
     public ParkingLot getParkingLot(List<ParkingLot> parkingLotList) {
-        return parkingLotList.stream().filter(lot -> lot.getTicketCarMap().size()!=lot.getMax()).findAny()
+        return parkingLotList.stream().filter(lot -> lot.getTicketCarMap().size()!=lot.getMax()).findFirst()
                 .orElseThrow(() -> new RuntimeException(NOT_ENOUGH_POSITION));
     }
 
     public Car fetch(ParkingTicket parkingTicket) throws RuntimeException {
-        if(parkingTicket != null){
-            ParkingLot parkingLot = parkingLotList.stream().filter(lot -> lot.getTicketCarMap().containsKey(parkingTicket)).findAny()
-                    .orElseThrow(() -> new RuntimeException(UNRECOGNIZE_PARKING_TICKET));
-            return  parkingLot.fetch(parkingTicket);
-        }
-        throw new RuntimeException(PLEASE_PROVIDE_YOUR_PARKING_TICKET);
+        Optional.ofNullable(parkingTicket).orElseThrow(() -> new RuntimeException(PLEASE_PROVIDE_YOUR_PARKING_TICKET));
+        ParkingLot parkingLot = parkingLotList.stream().filter(lot -> lot.getTicketCarMap().containsKey(parkingTicket)).findAny()
+                .orElseThrow(() -> new RuntimeException(UNRECOGNIZE_PARKING_TICKET));
+        return  parkingLot.fetch(parkingTicket);
     }
 
     public Car fetch(){
